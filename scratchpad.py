36976de180136acca0bc67f5954c2160224096d3
David Dansby <ddansby89@gmail.com>
2020-01-29 22:13:18 -0800

    more scratch

--- scratchpad.py
+++ scratchpad.py
@@ -150,11 +150,11 @@ async def print_after(message, delay):
 async def main():
     # Start coroutine twice (hopefully they start!)
     print(f"started at {time.strftime('%X')}")
-    first_awaitable = asyncio.create_task(print_after("world!", 5))
-    second_awaitable = asyncio.create_task(print_after("Hello", 1))
+    asyncio.create_task(print_after("world!", 5))
+    asyncio.create_task(print_after("Hello", 1))
     # Wait for coroutines to finish
-    await first_awaitable
-    await second_awaitable
+    # await first_awaitable
+    # await second_awaitable
     print(f"finished at {time.strftime('%X')}")
 
 
@@ -178,3 +178,15 @@ async def main():
 
 
 asyncio.run(main())
+
+
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.datetime.now())
+        if (loop.time() + 1.0) >= end_time:
+            break
+        await asyncio.sleep(1)
+
+asyncio.run(display_date())
